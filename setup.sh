bd811e643fb094608752552fe839ba24d1245c8e
Leo Palmer Sunmo <leo.sunmo@gmail.com>
2020-10-09 11:34:26 +1300

    Update setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [[ $(id -u) != 0 ]]; then
 fi
 
 # Add ppa for i3-regolith
-add-apt-repository -y ppa:/kgilmer/regolith-stable
+add-apt-repository ppa:regolith-linux/release
 
 # Add ppa for alacritty
 add-apt-repository -y ppa:mmstick76/alacritty
@@ -44,19 +44,6 @@ curl -L git.io/antigen > ~/.oh-my-zsh/custom/tools/antigen.zsh
 # Change default shell to ZSH
 chsh -s /bin/zsh
 
-# Install some applications
-
-# Download and install latest playerctl
-
-pushd /tmp/
-curl -s https://api.github.com/repos/acrisci/playerctl/releases/latest \
-| jq -r '.assets[] | select(.name | test("playerctl.+_amd64.deb")) | .browser_download_url' \
-| wget -qi -
-
-dpkg -i playerctl*_amd64.deb
-
-popd
-
 # Download latest release of bat
 pushd /tmp/
 curl -s https://api.github.com/repos/sharkdp/bat/releases/latest \
@@ -73,19 +60,18 @@ cp -R ./.config/bat ~/.config
 # Update bat's binary cache with new Nord theme
 bat cache --build
 
+# Install ASDF
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+pushd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
+popd
 
-# Install Kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl
+# Copy global ASDF tool-versions file
+cp -R ./.tool-versions ~/
 
+# Install ASDF tools
+asdf install
 
-# Install Krew (Kubectl plugin manager)
-(
-  set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://storage.googleapis.com/krew/v0.2.1/krew.{tar.gz,yaml}" &&
-  tar zxvf krew.tar.gz &&
-  ./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" install \
-    --manifest=krew.yaml --archive=krew.tar.gz
-)
 # Aliases and misc stuff
 git config --global alias.recent "for-each-ref --count=30 --sort=-committerdate refs/heads/ --format='%(refname:short)'"
 kubectl completion zsh > "${fpath[1]}/_kubectl"
