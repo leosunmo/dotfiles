2317af79a3b2e0f1b4aeb544d0f6903c28c8ea53
Leo Palmer <leps@boozt.com>
2025-03-14 09:48:06 +0100

    Add xrandr screen layouts

---
+++ .screenlayout/home-laptop-left.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x720 --rotate normal --scale 1x1 --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-3-1 --mode 2560x1440 --pos 3840x0 --rotate normal --scale 2x2 --output DP-3-2 --off --output DP-3-3 --mode 1920x1200 --pos 8960x480 --rotate normal --scale 2x2
